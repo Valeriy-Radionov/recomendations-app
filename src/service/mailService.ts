14c785b75fc00de5ccb8b722e8253a7510607e8f
valera <traberker901@gmail.com>
2022-12-21 18:47:06 +0300

    fetures: add sendergrid

--- src/service/mailService.ts
+++ src/service/mailService.ts
@@ -1,4 +1,6 @@
 import nodemailer from "nodemailer"
+import sgMail from "@sendgrid/mail"
+
 export const mailService = {
   async transporter() {
     const transporter = nodemailer.createTransport({
@@ -27,17 +29,72 @@ export const mailService = {
   },
 
   async sendFeedBackMail(from: string, senderName: string, message: string) {
-    const tramsporter = await this.transporter()
-    tramsporter.sendMail({
-      from: process.env.SMTP_USER,
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
+    const msg = {
       to: process.env.SMTP_USER_FEEDBACK_FORM,
+      from: from,
       subject: "HR MESSAGE",
-      text: "",
+      text: "message:",
       html: `<div>
-      <h1>Вам пришло письмо от ${senderName}</h1>
-      <p>Email: ${from}</p>
-      <p>${message}</p>
-      </div>`,
-    })
+              <h1>Вам пришло письмо от ${senderName}</h1>
+             <p>Email: ${from}</p>
+            <p>${message}</p>
+           </div>`,
+    }
+    try {
+      await sgMail.send(msg)
+    } catch (e) {
+      console.log(e)
+    }
   },
+
+  //   async senderGridTransporter() {
+  //     let transporter = nodemailer.createTransport({
+  //       host: "smtp.sendgrid.net",
+  //       port: 587,
+  //       auth: {
+  //         user: "AIrM4cQzQqi33KRDgrtvBA",
+  //         pass: process.env.SENDGRID_API_KEY,
+  //       },
+  //     })
+  //     return transporter
+  //   },
+  //   async sendFeedBackMail(from: string, senderName: string, message: string) {
+  //     const tramsporter = await this.senderGridTransporter()
+  //     tramsporter.sendMail(
+  //       {
+  //         from: from,
+  //         to: process.env.SMTP_USER_FEEDBACK_FORM,
+  //         subject: "HR MESSAGE",
+  //         text: "",
+  //         html: `<div>
+  //            <h1>Вам пришло письмо от ${senderName}</h1>
+  //           <p>Email: ${from}</p>
+  //          <p>${message}</p>
+  //          </div>`,
+  //       },
+  //       function (error, info) {
+  //         if (error) {
+  //           console.log(error)
+  //         } else {
+  //           console.log("Email sent: " + info.response)
+  //         }
+  //       }
+  //     )
+  //   },
+  // }
+  //   async sendFeedBackMail(from: string, senderName: string, message: string) {
+  //     const tramsporter = await this.transporter()
+  //     tramsporter.sendMail({
+  //       from: process.env.SMTP_USER,
+  //       to: process.env.SMTP_USER_FEEDBACK_FORM,
+  //       subject: "HR MESSAGE",
+  //       text: "",
+  //       html: `<div>
+  //       <h1>Вам пришло письмо от ${senderName}</h1>
+  //       <p>Email: ${from}</p>
+  //       <p>${message}</p>
+  //       </div>`,
+  //     })
+  //   },
 }
